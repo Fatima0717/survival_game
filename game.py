1e6ca3cdf792ab73dda484956c497b3e4b70ad6f
Fatima0717 <olayaescavias@gmail.com>
2024-06-18 08:54:23 +0900

    commit

--- game.py
+++ game.py
@@ -3,14 +3,14 @@ import random
 def show_instructions():
     print("""
     Welcome to the Adventure Game!
-    Find the treasure hidden in one of the rooms.
+    Your mission: Find the reasure hidden in one of the rooms. Beware of the unexpected!
 
     Commands:
-      go [direction]
-      get [item]
-      look
-      use [item]
-      quit
+      go [direction] - Move to another room (north, south, east, west)
+      get [item] - Pick up an item
+      look - Describe the current room
+      use [item] - Use an item from your inventory
+      quit - Exit the game 
     """)
 
 def show_status():
@@ -23,14 +23,14 @@ def show_status():
 
 def describe_room(room):
     describe = {
-        'Hall': "The hall is grand with a high celling and a large chandelier.",
-        'Kitchen': "The kitchen is messy with pots and pans everywhere.",
-        'Dining Room': "The dining room has a large table set for a feast.",
-        'Garden': "The garden is lush with flowers and a small pond.",
-        'Library': "The library is quiet with walls lined with books.",
-        'Basement': "The basement is dark and musty, filled with old furniture."
+        'Hall': "The hall is grand with a high celling and a large chandelier that makes you feel small. A talking portrait hangs on the wall.",
+        'Kitchen': "The kitchen is messy with pots and pans everywhere. A chef ghost is cooking up some invisible stew.",
+        'Dining Room': "The dining room has a large table set for a feast, but no one is around. A singing fish is mounted on the wall.",
+        'Garden': "The garden is lush with flowers and a small pond. A gnome is fishing and whiseling a merry tune",
+        'Library': "The library is quiet with walls lined with books. A floating bool is reading itself out loud.",
+        'Basement': "The basement is dark and musty, filled with old furniture. A friendly spider waves at you "
     }
-    return descriptions.get(room, "This place is indescribable.")
+    return descriptions.get(room, "This place is indescribable. It's just too weird!")
 
 # Define the game rooms
 rooms = {
@@ -91,7 +91,13 @@ while True:
             current_room = rooms[current_room][direction]
             # Random events
             if random.choice([True, False]):
-                print("A mysterious wind blows through the room...")
+                event = random.choice([
+                    "A mysterious wind blows through the room... Spookey!",
+                    "You hear adistant laugh... Meow!",
+                    "A ghost cat walks through the wall... Meow!",
+                    "You find a piece of candy on the fooor.. Yum!"
+                ])
+                print(event)
         else:
             print("You can't go that way!")
 
@@ -109,31 +115,20 @@ while True:
 
     elif command == 'use' and len(move) > 1:
         item = move[1]
-        if "item" in rooms[current_room] and item == rooms[current_room]['item']:
-            inventory.append(item)
-            print(f'{item} got!')
-            del rooms[current_room]['item']
+        if item == 'potion':
+            print("You drink the potion and feel refreshed! Yum!")
+            inventory.remove(item)
+        elif item == 'map':
+                print("You look at the map and get a better sense of the layout. very helpful")
+        elif item == 'flashlight':
+                print(f"You turn on the flashlight and can see better sense of the layout. Much better!")
         else:
-            print(f"Can't get {item}!")
-
-    elif command == 'look':
-        print(describe_room(current_room))
-    
-    elif command == 'use' and len(move) > 1:
-        item = move[1]
-        if item in inventory:
-            if item == 'potion':
-                print("You drink the potion and feel refreshed!")
-                inventory.remove(item)
-            elif item == 'map':
-                print("You look at the map and get better sense of the layout.")
-            elif item == 'flashlight':
-                print("You trun on the flashlight and can see better in the dark.")
-            else:
-                print(f"You don't have a {item} in your inventory")
+                print(f"You can't use the {item} here.")
+    else:
+        print(f"You don't have a {item} in your inventory.")
 
     elif command == 'quit':
-        print("Thanks for playing!")
+        print("Thanks for playing! come back soon!")
         break 
 
     else:
