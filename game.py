952ae851b0b548b270dd42dea45e33318f8636ae
Fatima0717 <olayaescavias@gmail.com>
2024-06-23 15:15:05 +0900

    create

--- game.py
+++ game.py
@@ -2,28 +2,66 @@ import random
 
 def show_instructions():
     print("""
-    
-          """)
+    welcome to Takuya room!
+    your mission: find the tresure hidden in one iif the rooms. Beware of the unxpected!
+
+    commands:
+     go [direction] - move to another room
+     get [item] - pick up an item
+     look - 
+     use [item]
+     quit - Exit the game
+    """)
 
 def show_status():
+    print('')
+    print(f'you are in the {current_room}')
+    print(f'Inventory: {inventory})
+    if "item in rooms [current_room]:
+          print(f'You see a [rooms[current_room]["item"]])
+    print('')
 
 def describe_room(room):
-    descriptions = {
 
+    descriptions = {
+        'Hall': "Hall is shool hoall"
+        'Kitchen': "New Kitchen"
+        'Dinig Room': "Huge room"
+        'Garden': "big"
+        'Library': "small"
+        'Basement': "undergrand"
     }
-    return descriptions.get(room,)
+    return descriptions.get(room, "This place is indescribable. It's just too weird!")
 
 rooms = {
     'Hall': {
-        'south': 'kitchen'
-    }
+        'south': 'Kitchen',
+        'east': 'Dinig Room',
+        'north': 'Library',
+        'west': 'Basement',
+        'item': 'key',
+    },
     'Kitchen': {
-
+        'north': 'Hall',
+        'item': 'monster'
+    },
+    'Dining Room': {
+        'west': 'Hall',
+        'south': 'Garden',
+        'item': 'potion'
+    },
+    'Garden': {
+        'north': 'Dininf Room',
+        'item': 'treasure'
+    },
+    'Library': {
+        'south': 'Hall',
+        'item': 'map'
+    },
+    'Basement': {
+        'east': 'Hall',
+        'item': 'flashlight'
     }
-    'Dinig room':{
-        'west':
-    }
-
 }
 
 current_room = 'Hall'
@@ -33,34 +71,65 @@ show_instructions()
 
 while True:
     show_status()
-    
+
     move = input('>').lower().split()
 
     if len(move) == 0:
         print("Invalid command. Try again.")
         continue
 
-    command = move
+    command = move[0]
 
     if command == 'go' and len(move) > 1:
         direction = move[1]
-        if direction in rooms[current_room]
+        if directon in rooms[current_room]:
             current_room = rooms[current_room][direction]
 
-            if random.choice([True,False]):
-                event = random.choice([
-
+            if random.choice([True, False]):
+                event =random.choice([
+                    "A mysterious Spooky!",
+                    "You hear a distant",
+                    "A gost"
+                    "You find"
                 ])
                 print(event)
         else:
             print("You can't go that way!")
-        
-    elif command == 'get' and len(move) > 1:
+
+    elif command =='get' and len(move) > 1:
         item = move[1]
-        if "item" in rooms[current_room]and item == rooms[current_room]['item']:
+        if "item" in rooms[current_room] and item == rooms[current_room]['item']:
             inventory.append(item)
             print(f'{item} got!')
             del rooms[current_room]['item']
         else:
             print(f"can't get {item}!")
-    elif 
+    elif command == 'look':
+        print(describe_room(current_room))
+
+    elif command == 'use' and len(move) > 1:
+        item = move[1]
+        if item in inventory:
+            if item == 'potion':
+                print("You drink the potion")
+                inventory.remove(item)
+            elif item == 'map':
+                print("You look at the map")
+            elif item =='flashlight':
+                print("You turn on the flashlight")
+            else:
+                print(f"You don't have a {item} in your inventory")
+
+    elif command == 'quite':
+        print("Thank for playing! come back soon!")
+        break
+    else:
+        print("Invalid command.Try again")
+    
+    if 'treasure' in inventory:
+        print("Congratulation!")
+        break
+
+    if 'monstor' in inventory:
+        print("A monster has caught you !")
+        break
